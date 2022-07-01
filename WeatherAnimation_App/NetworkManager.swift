a0da5b9cf9fbed470803a9fcdd2d4942ede49bfa
Felix Titov <titow.feliks@yandex.ru>
2022-07-02 00:17:57 +0300

    Created APIManager Completion

--- WeatherAnimation_App/NetworkManager.swift
+++ WeatherAnimation_App/NetworkManager.swift
@@ -12,10 +12,17 @@ import Foundation
 typealias JSONTask = URLSessionDataTask
 typealias JSONCompletionHandler = ([String: AnyObject]?, HTTPURLResponse?, Error?) -> Void
 
+enum APIResult<T> {
+    case success(T)
+    case failure(Error)
+}
+
 protocol NetworkManager {
     var sessionConfiguration: URLSessionConfiguration { get }
     var session: URLSession { get }
     
     init(sessionConfiguration: URLSessionConfiguration)
+    
     func JSONTask(with request: URLRequest, completionHandler: JSONCompletionHandler) -> JSONTask
+    func fetch<T>(request: URLRequest, parse: ([String: AnyObject]) -> T?, completionHandler: (APIResult<T>) -> Void)
 }
