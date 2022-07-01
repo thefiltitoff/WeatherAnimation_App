40aeedfac33f791c9ae2c433f1b750e17842f2a3
Felix Titov <titow.feliks@yandex.ru>
2022-07-01 23:10:24 +0300

    Created IBOutlets

--- WeatherAnimation_App/ViewController.swift
+++ WeatherAnimation_App/ViewController.swift
@@ -11,11 +11,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var pressureLabel: UILabel!
+    @IBOutlet weak var hummidityLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var appearentTemperatureLabel: UILabel!
+    @IBOutlet weak var refreshButton: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func refreshButtonTapped(_ sender: UIButton) {
+    }
+    
 }
 
