ba3a65b1fcfa47d0c09d82d0100647a77c4363be
Felix Titov <titow.feliks@yandex.ru>
2022-07-02 02:02:54 +0300

    Added SpriteKit

---
+++ WeatherAnimation_App/ParticlesView.swift
@@ -0,0 +1,14 @@
+//
+//  ParticlesView.swift
+//  WeatherAnimation_App
+//
+//  Created by Felix Titov on 7/2/22.
+//  Copyright © 2022 by Felix Titov. All rights reserved.
+//  
+
+
+import SpriteKit
+
+class ParticlesView: SKView {
+
+}
