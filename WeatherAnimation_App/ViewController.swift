020bad9889cfeb1b8f1707442c4c2d9491de71ff
Felix Titov <titow.feliks@yandex.ru>
2022-07-01 22:22:00 +0300

    Initial Commit

---
+++ WeatherAnimation_App/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  WeatherAnimation_App
+//
+//  Created by Felix Titov on 7/1/22.
+//  Copyright © 2022 by Felix Titov. All rights reserved.
+//  
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
