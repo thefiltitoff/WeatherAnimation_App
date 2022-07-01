4d5aef96bd49a9b67cf6f48bd37aef1221d609c4
Felix Titov <titow.feliks@yandex.ru>
2022-07-02 02:18:13 +0300

    Created rain effect

--- WeatherAnimation_App/ParticlesView.swift
+++ WeatherAnimation_App/ParticlesView.swift
@@ -10,5 +10,28 @@
 import SpriteKit
 
 class ParticlesView: SKView {
-
+    override func didMoveToSuperview() {
+        let scene  = SKScene(size: self.frame.size)
+        scene.backgroundColor = UIColor.clear
+        self.presentScene(scene)
+        
+        self.allowsTransparency = true
+        self.backgroundColor = UIColor.clear
+        
+        if let particles = SKEmitterNode(fileNamed: "ParticleScene.sks") {
+            particles.position = CGPoint(
+                x: self.frame.size.width / 2,
+                y: self.frame.size.height
+            )
+            
+            particles.particlePositionRange = CGVector(dx: self.bounds.size.width, dy: 0)
+            
+            addChild(for: scene, with: particles)
+            
+        }
+    }
+    
+    private func addChild(for scene: SKScene, with child: SKEmitterNode) {
+        scene.addChild(child)
+    }
 }
